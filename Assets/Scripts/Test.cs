58426ed944f42f431389cd466749eb6db2be60b6
jiepengtan <jiepengtan@gmail.com>
2019-06-26 10:58:48 +0800

    [+] add lockstep common libs

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,18 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Lockstep.Math;
+using Debug = Lockstep.Logging.Debug;
 
-public class Test : MonoBehaviour
-{
+public class Test : MonoBehaviour {
+    public LVector2 srcPoint;
+    public LVector2 dstPoint;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start(){
+        UnityEngine.Debug.Log("srcPoint " + srcPoint + " srcPoint" + dstPoint);
     }
 }
